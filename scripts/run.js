62346c173997e1ff3dc18f55f4fde3010ee2dc83
Fábio Matias <fabio_mcm2@hotmail.com>
2022-02-21 15:27:56 +0000

    section-1: register domains

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,17 @@
 const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
   const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  const txn = await domainContract.register("dev#1");
+  await txn.wait();
+
+  const domainOwner = await domainContract.getAddress("dev#1");
+  console.log("Owner of domain (dev#1):", domainOwner);
 };
 
 const runMain = async () => {
